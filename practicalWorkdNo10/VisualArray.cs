fb014343d71e1091f18e8315bcb9a1d3b0ea7ea8
Короче, Максон <102228515+HackFoxYeah@users.noreply.github.com>
2022-11-29 16:26:29 +0300

    Add files via upload

---
+++ practicalWorkdNo10/VisualArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Пример_таблицы_WPF;
+
+static class VisualArray
+{
+    /// <summary>
+    /// Реализует визуализацию даннных на DataGrid из передаваемого списка структуры PcInfo.
+    /// </summary>
+    /// <param name="pcList">Список содержащий экземпляры структуры PcInfo.</param>
+    /// <returns>Представление таблицы.</returns>
+    public static DataTable ToDataTable(this List<Int32> list)
+    {
+        DataTable tempDataGrid = new();
+        const int GridLength = 26;
+
+        for (int i = 0; i < GridLength; i++)
+        {
+            tempDataGrid.Columns.Add("#" + (i + 1), typeof(int));
+        }
+
+        DataRow row = tempDataGrid.NewRow();
+
+        for (int i = 0, j = 0; i < list.Count; i++, j++)
+        {
+            if (i % GridLength == 0 && i != 0)
+            {
+                tempDataGrid.Rows.Add(row);
+                row = tempDataGrid.NewRow();
+                j = 0;
+                row[j] = list[i];
+            }
+            else
+                row[j] = list[i];
+        }
+
+        tempDataGrid.Rows.Add(row);
+
+        return tempDataGrid;
+    }
+}
